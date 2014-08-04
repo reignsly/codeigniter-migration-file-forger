88990db8943043517f7c1f66c327f0cf8045c2b6
reignsly <sylvester.palitog@gmail.com>
2014-08-04 23:26:27 +0800

    Version 1

---
+++ application/controllers/home.php
@@ -0,0 +1,217 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->dbutil();
+	}
+
+	public function index()
+	{
+
+		$data = array();
+
+		$data['tables'] = $tables = ($this->db->list_tables());
+
+		$data['result'] = false;
+
+		if($tables){
+
+			foreach ($tables as $key => $tablename) {
+
+				$prefs = array(
+		                'tables'      => array($tablename),  // Array of tables to backup.
+		                'ignore'      => array(),           // List of tables to omit from the backup
+		                'format'      => 'sql',             // gzip, zip, txt
+		                'filename'    => 'mybackup.sql',    // File name - NEEDED ONLY WITH ZIP FILES
+		                'add_drop'    => false,              // Whether to add DROP TABLE statements to backup file
+		                'add_insert'  => false,              // Whether to add INSERT data to backup file
+		                'newline'     => "\r\n"               // Newline character used in backup file
+		              );
+
+				$backup =& $this->dbutil->backup($prefs); 
+
+				$file = $backup;
+
+				$fields     = '';
+				$primarykey = '';
+				$otherkey   = '';
+				$table      = '';
+
+				// echo 'if(!$this->db->table_exists("'.$tablename.'")){';
+				// echo "<br/>";
+
+				$contents = explode("\n",$file);
+				foreach ($contents as $key=>$line) {
+
+					$line = trim($line);
+
+					if (preg_match('/CREATE\sTABLE\sIF\sNOT\sEXISTS\s\`(.*?)\`/i',$line,$tablematch))
+					{
+						$table .= '$this->dbforge->create_table('."'".$tablematch[1]."'".');'.PHP_EOL."<br/>";
+						continue;
+					}
+
+					if (preg_match('/^`(.*?)`\s(.*?),$/i',$line,$aMatch)) {
+						$fields .='$this->dbforge->add_field("'.$aMatch[1].' '.$aMatch[2].'");'.PHP_EOL."<br/>";;
+					}
+
+					// lets get our keys and such!
+					if (preg_match('/PRIMARY\sKEY\s+\(\`(.*?)\`\)/i',$line,$primary)) {
+						$primarykey .= '$this->dbforge->add_key('."'".$primary[1]."'".', TRUE);'.PHP_EOL."<br/>";
+					}
+					if (preg_match('/KEY\s\`(.*?)\`/i',$line,$indexkey)) {
+						$otherkey .= '$this->dbforge->add_key('."'".$indexkey[1]."'".');'.PHP_EOL."<br/>";
+					}
+				}
+
+				// echo (stripslashes($fields));
+				// echo (stripslashes($primarykey));
+				// echo (stripslashes($otherkey));
+				// echo (stripslashes($table));
+				// echo '$this->dbforge->create_table("'.$tablename.'");';
+				// echo "<br/>";
+				// echo "}";
+				// echo "<br/>";
+				// echo "<br/>";
+
+			}
+		}
+
+		if($this->input->post('show_on_page')){
+
+			if($this->input->post('tables')){
+
+				$tables = $this->input->post('tables');
+
+				if($this->input->post('add_class_name')){
+					$data['result'] .= 'class Migration_'.$this->input->post('class_name').' extends CI_Migration {';
+					$data['result'] .= "\r\n";
+				}
+
+				if($this->input->post('add_up')){
+
+					$data['result'] .= 'public function up(){';
+					$data['result'] .= "\r\n";
+
+					foreach ($tables as $key => $tb) {
+						
+						$rs = $this->dump($tb);
+
+						$data['result'] .= 'if(!$this->db->table_exists("'.$tb.'")){';
+						$data['result'] .= "\r\n";
+
+						$data['result'] .= $rs->fields;					
+						$data['result'] .= $rs->primarykey;					
+						$data['result'] .= $rs->otherkey;					
+						$data['result'] .= $rs->table;					
+						$data['result'] .= '$this->dbforge->create_table("'.$tb.'");';
+						$data['result'] .= "\r\n";
+						$data['result'] .= "}";
+						$data['result'] .= "\r\n";
+					}
+
+					$data['result'] .= "}";
+				}
+
+				if($this->input->post('add_down')){
+
+					$data['result'] .= "\r\n";
+					$data['result'] .= 'public function down(){';
+					$data['result'] .= "\r\n";
+
+					foreach ($tables as $key => $tb) {
+						
+						$rs = $this->dump($tb);
+
+						$data['result'] .= 'if($this->db->table_exists("'.$tb.'")){';
+						$data['result'] .= "\r\n";
+						$data['result'] .= "\t".'$this->dbforge->drop_table("'.$tb.'");';
+						$data['result'] .= "\r\n";
+						$data['result'] .= "}";
+						$data['result'] .= "\r\n";
+					}
+
+					$data['result'] .= "}";
+				}
+
+				if($this->input->post('add_class_name')){
+					$data['result'] .= "\r\n";
+					$data['result'] .= "}";
+				}
+			}
+		}
+		
+		$this->load->view('home', $data);
+	}
+
+	private function dump($tablename){
+		$prefs = array(
+		                'tables'      => array($tablename),  // Array of tables to backup.
+		                'ignore'      => array(),           // List of tables to omit from the backup
+		                'format'      => 'sql',             // gzip, zip, txt
+		                'filename'    => 'mybackup.sql',    // File name - NEEDED ONLY WITH ZIP FILES
+		                'add_drop'    => false,              // Whether to add DROP TABLE statements to backup file
+		                'add_insert'  => false,              // Whether to add INSERT data to backup file
+		                'newline'     => "\r\n"               // Newline character used in backup file
+		              );
+
+		$backup =& $this->dbutil->backup($prefs); 
+
+		$file = $backup;
+
+		$fields     = '';
+		$primarykey = '';
+		$otherkey   = '';
+		$table      = '';
+
+		$rs = new stdClass();
+		$rs->fields = "";
+		$rs->primarykey = "";
+		$rs->otherkey = "";
+		$rs->table = "";
+
+		// echo 'if(!$this->db->table_exists("'.$tablename.'")){';
+		// echo "<br/>";
+
+		$contents = explode("\n",$file);
+		foreach ($contents as $key=>$line) {
+
+			$line = trim($line);
+
+			if (preg_match('/CREATE\sTABLE\sIF\sNOT\sEXISTS\s\`(.*?)\`/i',$line,$tablematch))
+			{
+				$rs->table .= '$this->dbforge->create_table('."'".$tablematch[1]."'".');'.PHP_EOL;
+				continue;
+			}
+
+			if (preg_match('/^`(.*?)`\s(.*?),$/i',$line,$aMatch)) {
+				$rs->fields .='$this->dbforge->add_field("'.$aMatch[1].' '.$aMatch[2].'");'.PHP_EOL;;
+			}
+
+			// lets get our keys and such!
+			if (preg_match('/PRIMARY\sKEY\s+\(\`(.*?)\`\)/i',$line,$primary)) {
+				$rs->primarykey .= '$this->dbforge->add_key('."'".$primary[1]."'".', TRUE);'.PHP_EOL;
+			}
+			if (preg_match('/KEY\s\`(.*?)\`/i',$line,$indexkey)) {
+				$rs->otherkey .= '$this->dbforge->add_key('."'".$indexkey[1]."'".');'.PHP_EOL;
+			}
+		}
+
+
+		return $rs;
+		// echo (stripslashes($fields));
+		// echo (stripslashes($primarykey));
+		// echo (stripslashes($otherkey));
+		// echo (stripslashes($table));
+		// echo '$this->dbforge->create_table("'.$tablename.'");';
+		// echo "<br/>";
+		// echo "}";
+		// echo "<br/>";
+		// echo "<br/>";
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/home.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Codeigniter Migration Forger</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
+	<style>
+		textarea {
+		    width: 100%;
+		    font-family: monospace; 
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<nav class="navbar navbar-inverse" role="navigation">
+			 <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">Codeigniter Migration Forger</a>
+		    </div>
+		</nav>
+		
+		<?=form_open('');?>
+
+		<?if($result):?>
+			<div class="row">
+			  <div class="col-lg-12">
+			  	<h4>Result</h4>
+			    <textarea class="form-control" rows="35" ><?=$result?></textarea>
+			  </div>
+			</div>
+			<br/>
+		<?endif;?>
+
+		<div class="panel panel-default">
+		  <div class="panel-heading">Options</div>
+		  <div class="panel-body">
+		    <table class="table table-bordered table-hover">
+		    	<thead>
+		    		<tr>
+		    			<td>Database Tables</td>
+		    			<td>Settings</td>
+		    		</tr>
+		    	</thead>
+		    	<tbody>
+		    		<tr>
+		    			<td>
+		    				<?if($tables):?>
+		    					<button onclick="check_all()" >Check All</button>
+		    					<button onclick="uncheck_all()">Un-check All</button>
+		    					<script>
+		    					function check_all(){
+		    						 $(".cb_tables").prop("checked", true);
+		    					}
+		    					function uncheck_all(){
+		    						 $(".cb_tables").prop("checked", false);
+		    					}
+		    					</script>
+								<?foreach ($tables as $key => $tb): ?>
+									<br/>
+									<input type="checkbox" class="cb_tables" name="tables[]" value="<?=$tb?>"><i class="fa fa-hand-o-right"></i>&nbsp; <?=$tb?>
+								<?endforeach;?>
+		    				<?else:?>
+		    					No Tables Available
+		    				<?endif;?>
+		    			</td>
+		    			<td>
+		    				
+		    				<div class="row">
+		    					<div class="col-md-12">
+									<h4>Show Result <small>on the page</small></h4>
+
+									<p>
+									<input type="checkbox" name="add_class_name" id="add_class_name" checked /> Add Class Name
+									<input type="text" name="class_name" id="class_name"></p>
+
+									<p><input type="checkbox" name="add_up" id="add_up" checked /> Add Function UP</p>
+									<p><input type="checkbox" name="add_down" id="add_down" checked /> Add Function DOWN</p>
+
+									<br/>
+									<input type="submit" value="Submit" name="show_on_page" class="btn btn-primary" />
+								</div>
+
+		    				</div>
+							<hr>
+		    				<div class="row">
+		    					
+		    				</div>
+
+		    			</td>
+		    		</tr>
+		    	</tbody>
+		    </table>
+		  </div>
+		</div>
+		<?=form_close();?>
+
+	</div>
+</body>
+</html>
